b40a2674b0c01b7e2afd6c34870750fa3c9a04f5
Christian Leonardo <pleasefloyd73@gmail.com>
2024-07-09 16:53:39 -0300

    feat: add controller to route request

--- src/main/routes/trip_routes.py
+++ src/main/routes/trip_routes.py
@@ -1,12 +1,25 @@
-from flask import jsonify, Blueprint
+from flask import jsonify, Blueprint, request
+
+# Controllers Imports
+from src.controllers.trip_creator import TripCreator
+
+# Repositories Imports
+from src.models.repositories.trips_repository import TripsRepository
+from src.models.repositories.emails_to_invite_repository import EmailsToInviteRepository
+
+from src.models.settings.db_connection_handler import db_connection_handler
+
 
 trips_routes_bp = Blueprint("trip_routes", __name__)
 
 
 @trips_routes_bp.route("/trips", methods=["POST"])
 def create_trip():
-    """
-    create a data trip
-    """
+    conn = db_connection_handler.get_connection()
+    trips_repository = TripsRepository(conn)
+    emails_to_invite_repository = EmailsToInviteRepository(conn)
+    controller = TripCreator(trips_repository, emails_to_invite_repository)
 
-    return jsonify({"message": "Hello World"}), 200
+    response = controller.create(request.json)
+
+    return jsonify(response["body"]), response["status_code"]
