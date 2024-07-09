bb674dd016e77b57abc3938e6d81b7fc6ad275d2
Christian Leonardo <pleasefloyd73@gmail.com>
2024-07-09 11:59:41 -0300

    feat: create connection db handler

---
+++ src/models/settings/db_connection_handler.py
@@ -0,0 +1,18 @@
+import sqlite3
+from sqlite3 import Connection
+
+
+class DBConnectionHandler:
+    def __init__(self) -> None:
+        self.__connection_string = "storage.db"
+        self.__conn = None
+
+    def connect(self) -> None:
+        conn = sqlite3.connect(self.__connection_string, check_same_thread=False)
+        self.__conn = conn
+
+    def get_connection(self) -> Connection | None:
+        return self.__conn
+
+
+db_connection_handler = DBConnectionHandler()
