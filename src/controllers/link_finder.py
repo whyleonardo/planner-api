dcc94512a84d0c60bfcdba3c0f14776046f5c0cb
Christian Leonardo <pleasefloyd73@gmail.com>
2024-07-10 10:07:14 -0300

    feat: create link finder controller

---
+++ src/controllers/link_finder.py
@@ -0,0 +1,24 @@
+from typing import Dict
+from src.models.repositories.links_repository import LinksRepository
+
+
+class LinkFinder:
+    def __init__(self, links_repository: LinksRepository) -> None:
+        self.__links_repository = links_repository
+
+    def find(self, trip_id) -> Dict:
+        try:
+            links = self.__links_repository.find_links_from_trip(trip_id)
+
+            formatted_links = []
+            for link in links:
+                formatted_links.append(
+                    {"id": link[0], "link": link[2], "title": link[3]}
+                )
+            return {"body": {"links": formatted_links}, "status_code": 200}
+
+        except Exception as exception:
+            return {
+                "body": {"error": "Bad Request", "message": str(exception)},
+                "status_code": 400,
+            }
